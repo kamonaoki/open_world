da4f768415293816ee8392e95f53231e4550a8af
kamonaoki <naokun616@gmail.com>
2024-07-17 17:48:15 +0900

    背景に動画を使用

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -582,3 +582,12 @@ function addMarker(post) {
   };
   image.src = post.image_url;
 }
+
+const menuToggle = document.querySelector(".toggle");
+const showcase = document.querySelector(".showcase");
+
+menuToggle.addEventListener("click", function () {
+  /* activeにする */
+  menuToggle.classList.toggle("active");
+  showcase.classList.toggle("active");
+});
